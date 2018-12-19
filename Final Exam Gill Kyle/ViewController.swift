ebffd835a2315a945d8cb7094dda837f1a21ea8c
gillkyle <kylerobertgill@gmail.com>
2018-12-19 07:09:29 -0700

    Initial Commit

---
+++ Final Exam Gill Kyle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final Exam Gill Kyle
+//
+//  Created by Kyle Gill on 12/19/18.
+//  Copyright © 2018 Kyle Gill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
